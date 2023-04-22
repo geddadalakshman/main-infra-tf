02312132f87aefb9c6d795db80017a8ea32391a1
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-04-22 12:06:02 +0530

    updated

--- main.tf
+++ main.tf
@@ -10,20 +10,20 @@ module "vpc" {
   public_subnets = each.value["public_subnets"]
   private_subnets = each.value["private_subnets"]
 }
-
-module "vpc" {
-  source = "git::https://github.com/geddadalakshman/docdb-module.git"
-  env = var.env
-  tags = var.tags
-
-  for_each = var.docdb
-  engine = each.value["engine"]
-  engine_version = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot = each.value["skip_final_snapshot"]
-  subnet_ids = local.db_subnet_ids
-}
+#
+#module "docdb" {
+#  source = "git::https://github.com/geddadalakshman/docdb-module.git"
+#  env = var.env
+#  tags = var.tags
+#
+#  for_each = var.docdb
+#  engine = each.value["engine"]
+#  engine_version = each.value["engine_version"]
+#  backup_retention_period = each.value["backup_retention_period"]
+#  preferred_backup_window = each.value["preferred_backup_window"]
+#  skip_final_snapshot = each.value["skip_final_snapshot"]
+#  subnet_ids = local.db_subnet_ids
+#}
 
 
 
