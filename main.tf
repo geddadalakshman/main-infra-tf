266af98028ba25f843004870d90960dcdb1257bc
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-04-25 18:23:42 +0530

    updated

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ module "alb" {
   name = each.value["name"]
   internal = each.value["internal"]
   load_balancer_type = each.value["load_balancer_type"]
-#  subnets = lookup(local.subnet_ids, each.value["subnet_name"], null )
+  subnets = local.subnet_ids
 }
 
 output "alb" {
